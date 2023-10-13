39fe2ca33b1a039b92a5eab1afdc6e7c4a0e1e20
srleg <frentuka@gmail.com>
2023-10-13 01:59:08 -0300

    Created concept of "Essentials" to categorize essential services that doesn't have dependencies.

--- src/main/kotlin/site/ftka/survivalcore/MClass.kt
+++ src/main/kotlin/site/ftka/survivalcore/MClass.kt
@@ -1,9 +1,13 @@
 package site.ftka.survivalcore
 
 import org.bukkit.plugin.java.JavaPlugin
+import site.ftka.survivalcore.essentials.logging.LoggingEssential
 import site.ftka.survivalcore.services.ServicesCore
 
 class MClass: JavaPlugin() {
+
+    // Initialize essentials
+    val loggingEssential = LoggingEssential(this)
 
     // Initialization (safe to initialize listeners)
     val onProxyInitRunnables: MutableList<Runnable> = mutableListOf()
@@ -11,13 +15,10 @@ class MClass: JavaPlugin() {
     val onProxyTermRunnables: MutableList<Runnable> = mutableListOf()
 
     // Initialize services
-    private lateinit var services: ServicesCore
-    fun services(): ServicesCore {
-        if (services == null) services = ServicesCore(this)
-        return services
-    }
+    lateinit var services: ServicesCore
 
     override fun onEnable() {
+        services = ServicesCore(this)
         onProxyInitRunnables.forEach(Runnable::run)
     }
 
