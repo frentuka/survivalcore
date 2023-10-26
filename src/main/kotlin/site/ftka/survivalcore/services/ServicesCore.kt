fb9c37609295fa0404770160efaafda445b0d5c2
srleg <frentuka@gmail.com>
2023-10-26 18:32:22 -0300

    Removed database service as it is no longer considered a service but an essential.

--- src/main/kotlin/site/ftka/survivalcore/services/ServicesCore.kt
+++ src/main/kotlin/site/ftka/survivalcore/services/ServicesCore.kt
@@ -1,28 +1,23 @@
 package site.ftka.survivalcore.services
 
-import kotlinx.coroutines.delay
 import site.ftka.survivalcore.MClass
-import site.ftka.survivalcore.services.database.dbService
 import site.ftka.survivalcore.services.language.LanguageService
 import site.ftka.survivalcore.services.permissions.PermissionsService
 import site.ftka.survivalcore.services.playerdata.PlayerDataService
 
 class ServicesCore(private val plugin: MClass) {
 
-    var dbService: dbService = dbService(plugin)
-    var playerDataService = PlayerDataService(plugin)
-    var languageService = LanguageService(plugin)
-    var permissionsService = PermissionsService(plugin)
+    var playerDataService = PlayerDataService(plugin, this)
+    var languageService = LanguageService(plugin, this)
+    var permissionsService = PermissionsService(plugin, this)
 
     fun initAll() {
-        dbService.init()
         playerDataService.init()
         languageService.init()
         permissionsService.init()
     }
 
     fun restartAll() {
-        dbService.restart()
         playerDataService.restart()
         languageService.restart()
         permissionsService.restart()
