1a69a58519bda9312f8798c4d9024e7d917e77ee
srleg <frentuka@gmail.com>
2023-11-06 19:57:50 -0300

    Removed col() functions as they are useless. Now using default Component system.

--- src/main/kotlin/site/ftka/survivalcore/utils/textUtils.kt
+++ src/main/kotlin/site/ftka/survivalcore/utils/textUtils.kt
@@ -1,32 +1,12 @@
 package site.ftka.survivalcore.utils
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextColor
 import java.util.*
 import java.util.function.Consumer
 
 object textUtils {
-    fun col(text: String): Component {
-        val text_char = text.toCharArray()
-        for (i in 0 until text_char.size - 1) {
-            if (text_char[i] == '&' && "0123456789AaBbCcDdEeFfKkLlMmNnOoRr".indexOf(text_char[i + 1]) > -1) {
-                text_char[i] = '\u00A7'
-                text_char[i + 1] = text_char[i + 1].lowercaseChar()
-            }
-        }
-        return Component.text(String(text_char))
-    }
-
-    fun col(text_list: List<String>): List<Component> {
-        val list: MutableList<Component> = ArrayList()
-        text_list.forEach(Consumer { text: String -> list.add(col(text)) })
-        return list
-    }
-
-    fun col(text_list: Array<String>?): List<Component> {
-        val list: MutableList<Component> = ArrayList()
-        Arrays.stream(text_list).forEach { text: String -> list.add(col(text)) }
-        return list
-    }
 
     fun asPlainText(arguments: Array<String>?): String {
         val sb = StringBuilder()
