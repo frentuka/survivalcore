eaba602df4062c86ecbd30955a955dd84aac7375
srleg <frentuka@gmail.com>
2023-11-06 19:56:49 -0300

    Included PlayerStateService.kt

--- src/main/kotlin/site/ftka/survivalcore/services/ServicesCore.kt
+++ src/main/kotlin/site/ftka/survivalcore/services/ServicesCore.kt
@@ -4,21 +4,25 @@ import site.ftka.survivalcore.MClass
 import site.ftka.survivalcore.services.language.LanguageService
 import site.ftka.survivalcore.services.permissions.PermissionsService
 import site.ftka.survivalcore.services.playerdata.PlayerDataService
+import site.ftka.survivalcore.services.playerstate.PlayerStateService
 
 class ServicesCore(private val plugin: MClass) {
 
     var playerDataService = PlayerDataService(plugin, this)
+    var playerStateService = PlayerStateService(plugin, this)
     var languageService = LanguageService(plugin, this)
     var permissionsService = PermissionsService(plugin, this)
 
     fun initAll() {
         playerDataService.init()
+        playerStateService.init()
         languageService.init()
         permissionsService.init()
     }
 
     fun restartAll() {
         playerDataService.restart()
+        playerStateService.restart()
         languageService.restart()
         permissionsService.restart()
     }
