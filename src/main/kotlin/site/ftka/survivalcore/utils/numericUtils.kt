dba0ac7f65340903c96b6e202e9f69dc52ab9fc9
srleg <frentuka@gmail.com>
2023-11-06 19:47:17 -0300

    Included roundDecimals function for Doubles and Floats.

--- src/main/kotlin/site/ftka/survivalcore/utils/numericUtils.kt
+++ src/main/kotlin/site/ftka/survivalcore/utils/numericUtils.kt
@@ -1,6 +1,7 @@
 package site.ftka.survivalcore.utils
 
 import java.util.concurrent.ThreadLocalRandom
+import kotlin.math.pow
 
 object numericUtils {
     fun getRandomInt(minInclusive: Int, maxInclusive: Int): Int {
@@ -9,5 +10,15 @@ object numericUtils {
 
     fun isIntegerParseable(textToVerify: String): Boolean {
         return textToVerify.chars().allMatch { codePoint: Int -> Character.isDigit(codePoint) }
+    }
+
+    fun Double.roundDecimals(decimalPlaces: Int): Double {
+        val factor = 10.0.pow(decimalPlaces.toDouble())
+        return Math.round(this * factor) / factor
+    }
+
+    fun Float.roundDecimals(decimalPlaces: Int): Float {
+        val factor = 10.0.pow(decimalPlaces.toDouble()).toFloat()
+        return Math.round(this * factor) / factor
     }
 }
