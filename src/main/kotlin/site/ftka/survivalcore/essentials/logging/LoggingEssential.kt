29c797ed96b11c75fe613e503f55cc4c7b041d6b
srleg <frentuka@gmail.com>
2024-01-23 23:15:33 -0300

    Log level check is now done by ServiceLog, so it has been removed from here.

--- src/main/kotlin/site/ftka/survivalcore/essentials/logging/LoggingEssential.kt
+++ src/main/kotlin/site/ftka/survivalcore/essentials/logging/LoggingEssential.kt
@@ -11,15 +11,8 @@ class LoggingEssential(private val plugin: MClass) {
 
     val logsFolderPath = plugin.dataFolder.absolutePath + "\\logs"
 
-    // Won't print logs with level above default.
-    private val printableLogLevel = LogLevel.DEBUG
-
-    fun print(tag: Component, log: Log) {
-        // Don't print higher-than-intended log levels.
-        if (log.level > printableLogLevel) return
-
+    fun print(tag: Component, log: Log) =
         plugin.server.consoleSender.sendMessage(tag.colorIfAbsent(NamedTextColor.AQUA).append(Component.text(" | ").color(NamedTextColor.DARK_GRAY).append(log.text.colorIfAbsent(NamedTextColor.YELLOW)))) // colorized
-    }
 
     fun getLog(serviceName: String, serviceTag: Component) = ServiceLogger(this, serviceName, serviceTag)
 
