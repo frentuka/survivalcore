4893a31b58e2eae99589d27f0e8bd35673eabdbd
srleg <frentuka@gmail.com>
2024-01-30 09:58:26 -0300

    Modified color parameters to be cleaner

--- src/main/kotlin/site/ftka/survivalcore/services/permissions/objects/PermissionGroup.kt
+++ src/main/kotlin/site/ftka/survivalcore/services/permissions/objects/PermissionGroup.kt
@@ -9,8 +9,8 @@ data class PermissionGroup(val uuid: UUID = UUID.randomUUID()) {
     var name = "NONE"
     var tag = "NONE"
     var category = GroupCategory.normal
-    var primaryColor = NamedTextColor.GRAY
-    var secondaryColor = NamedTextColor.DARK_GRAY
+    var primaryColor = NamedTextColor.GRAY.toString() //         to get: NamedTextColor.NAMES.value(primaryColor)
+    var secondaryColor = NamedTextColor.DARK_GRAY.toString() //  to get: NamedTextColor.NAMES.value(secondaryColor)
     var perms = setOf<String>()
     var inheritances = setOf<UUID>()
 
