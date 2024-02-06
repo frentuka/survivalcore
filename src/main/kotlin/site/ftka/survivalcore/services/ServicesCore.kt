5771f03763bccf1996f36ca39c5890112eba87d2
srleg <frentuka@gmail.com>
2024-02-06 17:44:19 -0300

    Included inventoryGUIService

--- src/main/kotlin/site/ftka/survivalcore/services/ServicesCore.kt
+++ src/main/kotlin/site/ftka/survivalcore/services/ServicesCore.kt
@@ -1,6 +1,7 @@
 package site.ftka.survivalcore.services
 
 import site.ftka.survivalcore.MClass
+import site.ftka.survivalcore.services.inventorygui.InventoryGUIService
 import site.ftka.survivalcore.services.language.LanguageService
 import site.ftka.survivalcore.services.permissions.PermissionsService
 import site.ftka.survivalcore.services.playerdata.PlayerDataService
@@ -10,17 +11,20 @@ class ServicesCore(private val plugin: MClass) {
     var playerDataService = PlayerDataService(plugin, this)
     var languageService = LanguageService(plugin, this)
     var permissionsService = PermissionsService(plugin, this)
+    var inventoryGUIService = InventoryGUIService(plugin, this)
 
     fun initAll() {
         playerDataService.init()
         languageService.init()
         permissionsService.init()
+        inventoryGUIService.init()
     }
 
     fun restartAll() {
         playerDataService.restart()
         languageService.restart()
         permissionsService.restart()
+        inventoryGUIService.restart()
     }
 
 }
