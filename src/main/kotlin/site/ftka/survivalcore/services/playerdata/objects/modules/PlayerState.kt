a82ad12581ff566094ed332ec84cae61ffa0188a
srleg <frentuka@gmail.com>
2024-02-06 17:45:34 -0300

    There was no need to create Doubles3DVector, as Triple<X, Y, Z> resolves it perfectly.

--- src/main/kotlin/site/ftka/survivalcore/services/playerdata/objects/modules/PlayerState.kt
+++ src/main/kotlin/site/ftka/survivalcore/services/playerdata/objects/modules/PlayerState.kt
@@ -7,7 +7,6 @@ import org.bukkit.entity.Player
 import org.bukkit.util.Vector
 import site.ftka.survivalcore.MClass
 import site.ftka.survivalcore.utils.base64Utils
-import site.ftka.survivalcore.utils.dataclasses.DoublesVector
 import java.util.UUID
 import site.ftka.survivalcore.utils.numericUtils.roundDecimals
 
@@ -30,8 +29,8 @@ data class PlayerState(private val uuid: UUID) {
     var experience: Float = 0f
 
     var world: String = "world" // Default world the player must be located in
-    var location: DoublesVector = DoublesVector(0.5, 100.1, 0.5) // default coordinates
-    var momentum: DoublesVector = DoublesVector(0.0, 0.0, 0.0) // default velocity
+    var location: Triple<Double, Double, Double> = Triple(0.5, 100.1, 0.5) // default coordinates
+    var momentum: Triple<Double, Double, Double> = Triple(0.0, 0.0, 0.0) // default velocity
     var fall_distance: Float = 0.0F
     var pitch_yaw: Pair<Float, Float> = Pair(0.0f, 0.0f)
 
@@ -54,8 +53,8 @@ data class PlayerState(private val uuid: UUID) {
         val loc = player.location
         val vel = player.velocity
         this.world = loc.world.name
-        this.location = DoublesVector(loc.x, loc.y, loc.z)
-        this.momentum = DoublesVector(vel.x, vel.y, vel.z)
+        this.location = Triple(loc.x, loc.y, loc.z)
+        this.momentum = Triple(vel.x, vel.y, vel.z)
         this.fall_distance = player.fallDistance
         this.pitch_yaw = Pair(loc.pitch.roundDecimals(2), loc.yaw.roundDecimals(2))
 
@@ -70,8 +69,8 @@ data class PlayerState(private val uuid: UUID) {
     }
 
     fun applyValuesToPlayer(plugin: MClass, player: Player) {
-        player.health = this.health; println("health set to ${this.health}")
-        player.foodLevel = this.foodLevel; println("food set to ${this.foodLevel}")
+        player.health = this.health
+        player.foodLevel = this.foodLevel
         player.saturation = this.saturation;
         player.exp = this.experience
 
@@ -86,7 +85,7 @@ data class PlayerState(private val uuid: UUID) {
             pend.setItem(item.key, base64Utils.fromBase64(item.value))
 
         // location stuff
-        val loc = Location(Bukkit.getWorld(this.world), this.location.x, this.location.y, this.location.z)
+        val loc = Location(Bukkit.getWorld(this.world), this.location.first, this.location.second, this.location.third)
 
         plugin.server.scheduler.runTaskLater(plugin, Runnable{
             loc.pitch = this.pitch_yaw.first
@@ -94,7 +93,7 @@ data class PlayerState(private val uuid: UUID) {
             player.teleport(loc)
             player.fallDistance = this.fall_distance
             plugin.server.scheduler.runTaskLater(plugin, Runnable{
-                player.velocity = Vector(this.momentum.x, this.momentum.y, this.momentum.z)
+                player.velocity = Vector(this.momentum.first, this.momentum.second, this.momentum.third)
             }, 1L)
         }, 1L)
 
