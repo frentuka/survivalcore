b0e307633252db6e15a4b0c8bbcb0269ad907477
srleg <frentuka@gmail.com>
2024-02-13 16:40:06 -0300

    Configuration file for PlayerData

---
+++ src/main/kotlin/site/ftka/survivalcore/essentials/configs/configurations/PlayerDataConfig.kt
@@ -0,0 +1,20 @@
+package site.ftka.survivalcore.essentials.configs.configurations
+
+import com.google.gson.GsonBuilder
+
+class PlayerDataConfig {
+
+    var version: Int = 1
+
+    val cache = cacheObj()
+    class cacheObj {
+        val timeToLiveMillis: Int = 1000*60*30
+        val clockLoopTimeSecs: Long = 10
+    }
+
+    fun toJson(): String {
+        val gsonPretty = GsonBuilder().setPrettyPrinting().create()
+        return gsonPretty.toJson(this)
+    }
+
+}
