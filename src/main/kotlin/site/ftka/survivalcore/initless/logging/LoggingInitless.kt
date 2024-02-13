996fc1329e4e2c24adc4bc80f94e8335a68d2242
srleg <frentuka@gmail.com>
2024-02-13 18:46:42 -0300

    Log messages alternate between two colors to improve readability. It's kinda buggy...

--- src/main/kotlin/site/ftka/survivalcore/initless/logging/LoggingInitless.kt
+++ src/main/kotlin/site/ftka/survivalcore/initless/logging/LoggingInitless.kt
@@ -2,6 +2,7 @@ package site.ftka.survivalcore.initless.logging
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextColor
 import site.ftka.survivalcore.MClass
 import site.ftka.survivalcore.initless.logging.objects.Log
 import site.ftka.survivalcore.initless.logging.objects.ServiceLogger
@@ -10,8 +11,20 @@ class LoggingInitless(private val plugin: MClass) {
 
     val logsFolderPath = plugin.dataFolder.absolutePath + "\\logs"
 
-    fun print(tag: Component, log: Log) =
-        plugin.server.consoleSender.sendMessage(tag.colorIfAbsent(NamedTextColor.AQUA).append(Component.text(" | ").color(NamedTextColor.DARK_GRAY).append(log.text.colorIfAbsent(NamedTextColor.YELLOW)))) // colorized
+    // below vars will switch one color to another for each message
+    // to improve readability
+    private val messageColorSwitch = Pair(TextColor.fromHexString("#FFFF44")!!, TextColor.fromHexString("#FFFFBB")!!)
+    private var currentMessageColor = true // true = first, false = second
+    fun print(tag: Component, log: Log) {
+        val msgColor = if (currentMessageColor) messageColorSwitch.first
+        else messageColorSwitch.second
+        currentMessageColor = !currentMessageColor
+
+        plugin.server.consoleSender.sendMessage(
+            tag.colorIfAbsent(NamedTextColor.AQUA).append(
+                Component.text(" | ").color(NamedTextColor.DARK_GRAY)
+                    .append(log.text.colorIfAbsent(msgColor)))) // colorized
+    }
 
     fun getLog(serviceName: String, serviceTag: Component) = ServiceLogger(this, serviceName, serviceTag)
 
