8fad72432b7855a520cd0157f7b8df9d984d2e35
srleg <frentuka@gmail.com>
2024-02-13 18:45:00 -0300

    Essentials moved to EssentialsFramework.kt

--- src/main/kotlin/site/ftka/survivalcore/MClass.kt
+++ src/main/kotlin/site/ftka/survivalcore/MClass.kt
@@ -1,29 +1,29 @@
 package site.ftka.survivalcore
 
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
-import site.ftka.survivalcore.apps.InventoryGUITester.invguitester
-import site.ftka.survivalcore.essentials.configs.ConfigsEssential
-import site.ftka.survivalcore.essentials.database.DatabaseEssential
+import site.ftka.survivalcore.essentials.EssentialsFramework
 import site.ftka.survivalcore.initless.logging.LoggingInitless
 import site.ftka.survivalcore.initless.metrics.MetricsInitless
 import site.ftka.survivalcore.initless.proprietaryEvents.ProprietaryEventsInitless
 import site.ftka.survivalcore.services.ServicesFramework
 import java.util.concurrent.Executors
 
-class MClass: JavaPlugin() {
+class MClass: JavaPlugin(), CommandExecutor {
 
     // basics
     val globalScheduler = Executors.newSingleThreadScheduledExecutor()
 
-    // Instantiate essentials
-    val configsEssential = ConfigsEssential(this)
-    val dbEssential = DatabaseEssential(this)
-
-    // Instantiate initless
+    // Instantiate initless-es
     val loggingInitless = LoggingInitless(this)
     val metricsInitless = MetricsInitless(this)
     val propEventsInitless = ProprietaryEventsInitless(this)
+
+    // Instantiate essentials
+    val essentialsFwk = EssentialsFramework(this)
 
     // Instantiate services
     val servicesFwk = ServicesFramework(this)
@@ -33,21 +33,24 @@ class MClass: JavaPlugin() {
 
     override fun onEnable() {
         // initialize all essentials
-        configsEssential.init()
-        dbEssential.init()
+        essentialsFwk.initAll()
+
+        // initialize services
         servicesFwk.initAll()
 
         initListeners()
 
+        // apps
+
         starting = false
 
-        val invgt = invguitester(this)
+        getCommand("restart")?.setExecutor(this)
     }
 
     override fun onDisable() {
         stopping = true
 
-        dbEssential.disconnect()
+        essentialsFwk.dbEssential.disconnect()
     }
 
     private val listenerList = mutableListOf<Listener>()
@@ -65,6 +68,16 @@ class MClass: JavaPlugin() {
             server.pluginManager.registerEvents(listener, this)
             listenerList.remove(listener)
         }
+    }
+
+    // restart command: 0000
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
+        if (label.lowercase() == "0000") {
+            this.server.pluginManager.disablePlugin(this)
+            this.server.pluginManager.enablePlugin(this)
+        }
+
+        return super.onCommand(sender, command, label, args)
     }
 
 }
