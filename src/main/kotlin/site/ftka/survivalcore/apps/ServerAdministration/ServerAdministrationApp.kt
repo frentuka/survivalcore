05c463e13adc7d1633abaa46ef9b7cb452e09609
srleg <frentuka@gmail.com>
2024-02-13 16:33:54 -0300

    Started ServerAdministration app. Will provide commands and GUI for full control of services and essentials.

---
+++ src/main/kotlin/site/ftka/survivalcore/apps/ServerAdministration/ServerAdministrationApp.kt
@@ -0,0 +1,14 @@
+package site.ftka.survivalcore.apps.ServerAdministration
+
+import site.ftka.survivalcore.MClass
+
+class ServerAdministrationApp(private val plugin: MClass) {
+
+    /*
+        Will provide commands and a GUI
+        for internal control of services and essentials
+
+     */
+
+
+}
