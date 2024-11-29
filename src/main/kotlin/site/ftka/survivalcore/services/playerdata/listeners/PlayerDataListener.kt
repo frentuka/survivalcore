b919c72cc404fe9ccdfcbf0d58ed08e6d4e910ac
frentuka <frentuka@gmail.com>
2024-11-29 17:19:53 -0300

    Removed  default join/quit messages.

--- src/main/kotlin/site/ftka/survivalcore/services/playerdata/listeners/PlayerDataListener.kt
+++ src/main/kotlin/site/ftka/survivalcore/services/playerdata/listeners/PlayerDataListener.kt
@@ -1,9 +1,9 @@
 package site.ftka.survivalcore.services.playerdata.listeners
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerKickEvent
 import org.bukkit.event.player.PlayerQuitEvent
@@ -11,6 +11,7 @@ import site.ftka.survivalcore.MClass
 import site.ftka.survivalcore.essentials.database.events.DatabaseDisconnectEvent
 import site.ftka.survivalcore.essentials.database.events.DatabaseReconnectEvent
 import site.ftka.survivalcore.initless.logging.LoggingInitless
+import site.ftka.survivalcore.initless.logging.LoggingInitless.*
 import site.ftka.survivalcore.initless.proprietaryEvents.annotations.PropEventHandler
 import site.ftka.survivalcore.initless.proprietaryEvents.enums.PropEventPriority
 import site.ftka.survivalcore.initless.proprietaryEvents.interfaces.PropListener
@@ -25,9 +26,12 @@ class PlayerDataListener(private val service: PlayerDataService, private val plu
     fun onPlayerJoin(event: PlayerJoinEvent) {
         val player_uuid = event.player.uniqueId
 
+        // disable join message
+        event.joinMessage(Component.text(""))
+
         // If database health is false, prevent player from joining
         if (!essFwk.database.health) {
-            event.player.kick(Component.text("PlayerData was unable to communicate with database"), PlayerKickEvent.Cause.UNKNOWN)
+            event.player.kick(Component.text("Unable to communicate with database"), PlayerKickEvent.Cause.UNKNOWN)
             return
         }
 
@@ -42,7 +46,10 @@ class PlayerDataListener(private val service: PlayerDataService, private val plu
     fun onPlayerQuit(event: PlayerQuitEvent) {
         val player = event.player
 
-        service.logger.log("Player quit event", LoggingInitless.LogLevel.DEBUG)
+        // disable quit message
+        event.quitMessage(Component.text(""))
+
+        service.logger.log("Player quit event", LogLevel.DEBUG)
 
         // Remove from online players
         service.onlinePlayers.remove(player.uniqueId)
@@ -56,7 +63,7 @@ class PlayerDataListener(private val service: PlayerDataService, private val plu
     // Make emergency dump for every player
     @PropEventHandler(priority = PropEventPriority.FIRST)
     fun onDatabaseHealthCheckFail(event: DatabaseDisconnectEvent) {
-        service.logger.log("Database health check failed. Dumping all playerdata into storage.", LoggingInitless.LogLevel.LOW)
+        service.logger.log("Database health check failed. Dumping all playerdata into storage.", LogLevel.LOW, NamedTextColor.RED)
 
         for (playerdata in service.playerDataMap.values) {
             service.emergency_ss.emergencyDump(playerdata)
@@ -66,17 +73,7 @@ class PlayerDataListener(private val service: PlayerDataService, private val plu
 
     @PropEventHandler(priority = PropEventPriority.FIRST)
     fun onDatabaseReconnect(event: DatabaseReconnectEvent) {
-        service.logger.log("Database reconnected. Saving all emergency dumps if available.", LoggingInitless.LogLevel.LOW)
+        service.logger.log("Database reconnected. Saving available emergency dumps.", LoggingInitless.LogLevel.LOW)
         service.emergency_ss.uploadAllDumpsToDatabase(false)
     }
-
-    /*
-        TESTING
-     */
-    @EventHandler
-    fun onMove(event: EntityDamageEvent) {
-
-    }
-
-
 }
