6c795b189a427d6f935844212901510a7faddf6f
frentuka <frentuka@gmail.com>
2024-11-29 14:57:55 -0300

    Updated whole project: Paper 1.20 -> Paper 1.21 Java 17 -> Java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "2.1.0"
+    id("java")
     id("com.gradle.plugin-publish") version "0.17.0"
     application
 }
@@ -8,30 +11,50 @@ group = "site.ftka"
 version = "1.0-SNAPSHOT"
 
 java {
-    targetCompatibility = JavaVersion.VERSION_17
-    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
     mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://papermc.io/repo/repository/maven-public/")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect")) // Needed for proprietary events
+    implementation("com.google.code.gson:gson:2.8.9")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.21")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
-    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT") // paper
+    // Adventure dependencies
+    implementation("net.kyori:adventure-api:4.14.0")
+    implementation("net.kyori:adventure-platform-bukkit:4.3.0")
+    implementation("net.kyori:adventure-text-serializer-gson:4.14.0")
+    implementation("net.kyori:adventure-text-serializer-plain:4.14.0")
+    implementation("net.kyori:adventure-text-serializer-legacy:4.14.0")
 
-    compileOnly("io.lettuce:lettuce-core:6.2.6.RELEASE") // redis
+    compileOnly("io.papermc.paper:paper-api:1.21.3-R0.1-SNAPSHOT") // Paper API
+
+    compileOnly("io.lettuce:lettuce-core:6.5.0.RELEASE") // redis
     implementation("org.apache.commons:commons-pool2:2.4.3") // connection pooling
 }
 
+
+
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "21"
+    }
+}
+
+tasks.wrapper<Wrapper> {
+    gradleVersion = "8.5"
 }
 
 tasks.withType<Jar> {
@@ -42,7 +65,7 @@ tasks.withType<Jar> {
         attributes["Main-Class"] = "site.ftka.proxycore.MClass"
     }
 
-    destinationDirectory = layout.buildDirectory.dir("C:/Users/srleg/Desktop/testing/survival/plugins")
+    destinationDirectory = layout.buildDirectory.dir("/home/srleg/Desktop/server/plugins/")
 
     from(sourceSets.main.get().output)
     dependsOn(configurations.runtimeClasspath)
