3900a817715c85f3387470e3a73c1165e00f90c9
srleg <frentuka@gmail.com>
2025-01-13 11:31:56 -0300

    UsernameTracker: If a player joins and there is another player with the exact same name, it is safe to assume that the joining player is the real owner of this name and that the "not joining" player has changed their name while online. So, "not joining" player will be kicked.

--- src/main/kotlin/site/ftka/survivalcore/essentials/usernameTracker/listeners/UsernameTrackerEssential_Listener.kt
+++ src/main/kotlin/site/ftka/survivalcore/essentials/usernameTracker/listeners/UsernameTrackerEssential_Listener.kt
@@ -3,14 +3,34 @@ package site.ftka.survivalcore.essentials.usernameTracker.listeners
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerLoginEvent
+import site.ftka.survivalcore.MClass
 import site.ftka.survivalcore.essentials.usernameTracker.UsernameTrackerEssential
 
-class UsernameTrackerEssential_Listener(private val ess: UsernameTrackerEssential): Listener {
+class UsernameTrackerEssential_Listener(private val ess: UsernameTrackerEssential, private val plugin: MClass): Listener {
 
     @EventHandler
-    fun onPreLogin(event: PlayerLoginEvent) {
+    fun onLogin(event: PlayerLoginEvent) {
         val uuid = event.player.uniqueId
         val name = event.player.name
+
+        /*
+        is a player with the exact same name already connected?
+        if it is, then it's name should really be it's old name
+        and the player who's connecting is the only one with that name for real
+         */
+
+        if (plugin.servicesFwk.playerData.data.getOnlinePlayers().containsValue(name)) {
+            val matchingUsernamesMap = plugin.servicesFwk.playerData.data.getOnlinePlayers().filterValues { it == name }.keys
+
+            for (matchingUUID in matchingUsernamesMap)
+                if (uuid != matchingUUID) {
+                    plugin.servicesFwk.playerData.data.removeOnlinePlayer(uuid)
+
+                    // todo: pretty this up. some sort of message is needed
+                    plugin.server.getPlayer(matchingUUID)?.player?.kick()
+                }
+        }
+
         ess.addUsername(uuid, name)
     }
 
